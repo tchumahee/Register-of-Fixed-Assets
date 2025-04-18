5104c35a86c08735bb52dca446576ce512ab5f08
Sofija Rodić <74682145+Sofija1234@users.noreply.github.com>
2025-04-18 23:38:08 +0200

    maps test

--- app/(tabs)/locations.tsx
+++ app/(tabs)/locations.tsx
@@ -1,12 +1,49 @@
 import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+
+const INITIAL_REGION = {
+  latitude: 43.9159,
+  longitude: 17.6791,
+  latitudeDelta: 3.0,
+  longitudeDelta: 5.0,
+}
+
+const locations = [
+  {
+    latitude: 43.8563,
+    longitude: 18.4131,
+    name: "Sarajevo"
+  },
+  {
+    latitude: 44.7722,
+    longitude: 17.1910,
+    name: "Banja Luka"
+  }
+]
+
+const markers = locations.map(loc => ({
+  ...loc,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01
+}));
+  
+
 
 export default function LocationsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Locations</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_GOOGLE}
+      initialRegion={INITIAL_REGION}
+      >
+        {
+          markers.map((marker, index) => (
+            <Marker key={index} coordinate={marker}/>
+          ))
+        }
+      </MapView>
     </View>
   );
 }
