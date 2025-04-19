77140da80bd7cc2ce05455e03492b16e2d069d35
Sofija Rodić <74682145+Sofija1234@users.noreply.github.com>
2025-04-19 15:39:44 +0200

    removing local styles

--- app/(tabs)/employees.tsx
+++ app/(tabs)/employees.tsx
@@ -84,24 +84,3 @@ export default function EmployeesScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // title: {
-  //   fontSize: 20,
-  //   fontWeight: 'bold',
-  // },
-  // text: {
-  //   fontSize: 20
-  // },
-  
-  // separator: {
-  //   marginVertical: 30,
-  //   height: 1,
-  //   width: '80%',
-  // },
-});
